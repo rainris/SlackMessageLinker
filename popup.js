4a4da430590141dc08cac3aa1b7823890a7e81b1
kwonojin <kwonojin@linecorp.com>
2024-04-10 10:51:31 +0900

    Add copy url button

--- popup.js
+++ popup.js
@@ -3,10 +3,8 @@ document.getElementById('copyInfoBtn').addEventListener('click', function () {
         chrome.tabs.sendMessage(tabs[0].id, { action: "collectInfo" }, function (response) {
             if (response.info) {
                 navigator.clipboard.writeText(response.info).then(() => {
-                    // 복사 성공 시 처리
                     alert('Information copied to clipboard!');
                 }, (err) => {
-                    // 복사 실패 시 처리
                     console.error('Failed to copy information: ', err);
                 });
             }
